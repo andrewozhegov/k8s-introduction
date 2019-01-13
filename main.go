782bf3c57c0ae9c3562257140b26365ed6bf96f5
andrew.ozhegov@yandex.ru <andrew.ozhegov@yandex.ru>
2019-01-14 01:58:15 +0300

    add logger

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+    "github.com/Sirupsen/logrus"
     "github.com/takama/router" // use $ go get to install it
     //"github.com/julienschmidt/httprouter" // good solution too
 )
+
+var log = logrus.New()
 
 // Run server: go build && k8s-introduction
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
 	r := router.New()
+    r.Logger = logger
 	r.GET("/", home)
 	r.Listen(":8000")
 }
