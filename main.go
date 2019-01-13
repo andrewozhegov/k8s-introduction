34a601065e84975b44414f99a5fc6a847ef84bd7
andrew.ozhegov@yandex.ru <andrew.ozhegov@yandex.ru>
2019-01-14 02:13:11 +0300

    ability to setup port as env var

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "os"
     "github.com/Sirupsen/logrus"
     "github.com/takama/router" // use $ go get to install it
     //"github.com/julienschmidt/httprouter" // good solution too
@@ -11,8 +12,13 @@ var log = logrus.New()
 // Run server: go build && k8s-introduction
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
-	r := router.New()
+    port := os.Getenv("SERVICE_PORT") // specify port when run: $ env SERVICE_PORT=8000 ./k8s-introduction
+	if len(port) == 0 {
+        log.Fatal("Service port in not specified")
+    }
+ 
+    r := router.New()
     r.Logger = logger
 	r.GET("/", home)
-	r.Listen(":8000")
+    r.Listen(":" + port) // or ("0.0.0.0:" + port)
 }
