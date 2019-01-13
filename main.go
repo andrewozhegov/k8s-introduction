db679330ae54eebeba6414f0a9bcf40a48ca203f
andrew.ozhegov@yandex.ru <andrew.ozhegov@yandex.ru>
2019-01-13 23:38:55 +0300

    using takama/router

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"net/http"
+    "github.com/takama/router" // use $ go get to install it
+    //"github.com/julienschmidt/httprouter" // good solution too
 )
 
 // Run server: go build && k8s-introduction
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
-	http.HandleFunc("/", home)
-	http.ListenAndServe(":8000", nil)
+	r := router.New()
+	r.GET("/", home)
+	r.Listen(":8000")
 }
