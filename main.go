00c139f4568750def4f2a31450f4e106e403bec0
andrew.ozhegov@yandex.ru <andrew.ozhegov@yandex.ru>
2019-01-14 16:36:43 +0300

    graceful shutdown & depends upd

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+    "net/http"
     "os"
+
     "github.com/Sirupsen/logrus"
+    common_handlers "github.com/k8s-community/handlers/info" // use $ glide get to add
+    "github.com/andrewozhegov/k8s-introduction/version"
+    "github.com/k8s-community/utils/shutdown"
     "github.com/takama/router" // use $ go get to install it
     //"github.com/julienschmidt/httprouter" // good solution too
 )
@@ -16,9 +21,29 @@ func main() {
 	if len(port) == 0 {
         log.Fatal("Service port in not specified")
     }
- 
+
     r := router.New()
     r.Logger = logger
 	r.GET("/", home)
-    r.Listen(":" + port) // or ("0.0.0.0:" + port)
+
+	// Readiness and liveness probes for Kubernetes
+	//r.GET("/info", func(c *router.Control) {
+	//	common_handlers.Info(c, version.RELEASE, version.REPO, version.COMMIT)
+	//})
+    r.GET("/info", common_handlers.Handler(version.RELEASE, version.REPO, version.COMMIT))
+	r.GET("/healthz", func(c *router.Control) {
+		c.Code(http.StatusOK).Body(http.StatusText(http.StatusOK))
+	})
+
+    go r.Listen(":" + port) // or ("0.0.0.0:" + port)
+
+    logger := log.WithField("event", "shutdown")
+	sdHandler := shutdown.NewHandler(logger)
+	sdHandler.RegisterShutdown(sd)
+}
+
+// sd does graceful shutdown of the service
+func sd() (string, error) {
+	// if service has to finish some tasks before shutting down, these tasks must be finished her	
+    return "Ok", nil
 }
