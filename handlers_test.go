e9c7c68c0a7ee9bdc20f84e12612f1339af79c35
andrew.ozhegov@yandex.ru <andrew.ozhegov@yandex.ru>
2019-01-16 03:27:18 +0300

    tests upd

--- handlers_test.go
+++ handlers_test.go
@@ -1,82 +1,64 @@
 package main
 
 import (
-    "bytes"
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
-    "github.com/Sirupsen/logrus"
-    "github.com/takama/router"
+	"github.com/andrewozhegov/k8s-introduction/version"
+	"github.com/takama/router"
 )
 
-// buffer is a special variable to save log messages
-var buffer bytes.Buffer
-
-func init() {
-	log.Out = &buffer
-	log.Formatter = &logrus.JSONFormatter{}
-}
-
-// TestHandler is the simplest test: check base (/) URL
-// Using: $ go test -v
-func TestHandler(t *testing.T) {
-    r := router.New()
-	r.GET("/", home)
-
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-
-	res, err := http.Get(ts.URL + "/")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	greeting, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expectedGreeting := "Processing URL /..."
-	testingGreeting := strings.Trim(string(greeting), " \n")
-	if testingGreeting != expectedGreeting {
-		t.Fatalf(
-			"Wrong greeting '%s', expected '%s'",
-			testingGreeting, expectedGreeting,
-		)
-	}
-}
-
-// TestLogger checks if logger handler works correctly
-func TestLogger(t *testing.T) {
+func TestHealthHandler(t *testing.T) {
 	r := router.New()
-	r.Logger = logger
-
-	ts := httptest.NewServer(r)
-	defer ts.Close()
-
-	_, err := http.Get(ts.URL + "/")
+	r.GET("/healthz", healthz)
+	req, err := http.NewRequest("GET", "/healthz", nil)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
+	trw := httptest.NewRecorder()
+	r.ServeHTTP(trw, req)
+	if trw.Body.String() != "Ok" {
+		t.Error("Expected", "Ok", "got", trw.Body.String())
+	}
+	if trw.Code != 200 {
+		t.Error("Expected status:", 200, "got", trw.Body.String())
+	}
+}
 
-	formated := struct {
-		Level string `json:"level"`
-		Msg   string `json:"msg"`
-		Time  string `json:"time"`
-	}{}
-	err = json.NewDecoder(&buffer).Decode(&formated)
+func TestInfoHandler(t *testing.T) {
+	r := router.New()
+	r.GET("/info", info)
+	req, err := http.NewRequest("GET", "/info", nil)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
+	trw := httptest.NewRecorder()
+	r.ServeHTTP(trw, req)
+	inf := "{\n  \"commit\": \"" + version.COMMIT + "\",\n  \"repo\": \"" + version.REPO +
+		"\",\n  \"version\": \"" + version.RELEASE + "\"\n}"
+	if trw.Body.String() != inf {
+		t.Error("Expected", inf, "got", trw.Body.String())
+	}
+	if trw.Code != 200 {
+		t.Error("Expected status:", 200, "got", trw.Body.String())
+	}
+}
 
-	msgParts := strings.Split(formated.Msg, " ")
-	if len(msgParts) != 3 {
-		t.Fatalf("Wrong message was logged: %s", formated.Msg)
+func TestRootHandler(t *testing.T) {
+	r := router.New()
+	r.GET("/", root)
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	trw := httptest.NewRecorder()
+	r.ServeHTTP(trw, req)
+	inf := "k8s-introduction v" + version.RELEASE
+	if trw.Body.String() != inf {
+		t.Error("Expected", inf, "got", trw.Body.String())
+	}
+	if trw.Code != 200 {
+		t.Error("Expected status:", 200, "got", trw.Body.String())
 	}
 }
