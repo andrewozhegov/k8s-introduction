c08e1ec582fe7a99f15c48232583b4c9a30bd7b9
andrewozhegov <andrew.ozhegov@yandex.ru>
2019-04-14 23:56:50 +0300

    separate handlers init & using gorilla/mux router

---
+++ handlers/handlers.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"fmt"
+    "log"
+    "net/http"
+	"sync/atomic"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func root(w http.ResponseWriter, _ *http.Request) {
+    fmt.Fprint(w, "Hello! Your request was processed.")
+}
+
+// Router register necessary routes and returns an instance of a router.
+func Router(version, commit, repo string) *mux.Router{
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+	go func() {
+		log.Printf("Readyz probe is negative by default...")
+		time.Sleep(10 * time.Second)
+		isReady.Store(true)
+		log.Printf("Readyz probe is positive.")
+	}()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", root).Methods("GET")
+	r.HandleFunc("/home", home(version, commit, repo)).Methods("GET")
+	r.HandleFunc("/healthz", healthz)
+	r.HandleFunc("/readyz", readyz(isReady))
+
+    return r
+}
